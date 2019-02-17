40e44e52dcb74352cc027c7f4d9c5378a268e9c5
gino <gino.mempin@gmail.com>
2019-02-17 15:37:50 +0900

    lint: Fix pylint issues

--- members/base.py
+++ members/base.py
@@ -14,19 +14,23 @@ def _get_member_list():
         return False
 
 
-member_list = _get_member_list()
-if not member_list:
-    print("ABORT. Cannot load data file.")
-else:
-    name = input("Enter name: ")
-    member = member_list.get(name.lower())
-    if member:
-        for field, info in member.items():
-            print("{0:<11} : {1:<}".format(field, info))
+def main():
+    member_list = _get_member_list()
+    if not member_list:
+        print("ABORT. Cannot load data file.")
     else:
-        print("There is no entry for '" + name + "'.")
-        possibles = get_close_matches(name, member_list.keys(), n=5, cutoff=0.75)
-        if possibles:
-            print("Did you mean:")
-            for p in possibles:
-                print("  {}?".format(p))
+        name = input("Enter name: ").lower()
+        member = member_list.get(name)
+        if member:
+            for field, info in member.items():
+                print("{0:<11} : {1:<}".format(field, info))
+        else:
+            print("There is no entry for '" + name + "'.")
+            possibles = get_close_matches(name, member_list.keys(), n=5, cutoff=0.60)
+            if possibles:
+                print("Did you mean:")
+                for pos in possibles:
+                    print("  {}?".format(pos))
+
+
+main()
--- timesheet/base.py
+++ timesheet/base.py
@@ -1,29 +1,34 @@
 
 import pandas
 
-print("READ timesheet.xlsx")
-df = pandas.read_excel("timesheet.xlsx", sheet_name="DATA")
-print("SIZE: {}".format(df.shape))
-print("ROWS: {}".format(df.index))
-print("COLS: {}".format(df.columns))
-print("=" * 60)
-print(df)
-print("=" * 60)
 
-print("UPDATE Name column")
-df["Name"] = df.loc[:, "Name"].apply(lambda n: n.title())
-print("=" * 60)
-print(df)
-print("=" * 60)
+def main():
+    print("READ timesheet.xlsx")
+    data = pandas.read_excel("timesheet.xlsx", sheet_name="DATA")
+    print("SIZE: {}".format(data.shape))
+    print("ROWS: {}".format(data.index))
+    print("COLS: {}".format(data.columns))
+    print("=" * 60)
+    print(data)
+    print("=" * 60)
 
-print("ADD OT column")
-df["OT"] = df.loc[:, "Total Hours"].apply(lambda h: h - 40)
-print("=" * 60)
-print(df)
-print("=" * 60)
+    print("UPDATE Name column")
+    data["Name"] = data.loc[:, "Name"].apply(lambda n: n.title())
+    print("=" * 60)
+    print(data)
+    print("=" * 60)
 
-print("DROP EmployeeID column")
-df.drop(columns=["EmployeeID"], inplace=True)
-print("=" * 60)
-print(df)
-print("=" * 60)
+    print("ADD OT column")
+    data["OT"] = data.loc[:, "Total Hours"].apply(lambda h: h - 40)
+    print("=" * 60)
+    print(data)
+    print("=" * 60)
+
+    print("DROP EmployeeID column")
+    data.drop(columns=["EmployeeID"], inplace=True)
+    print("=" * 60)
+    print(data)
+    print("=" * 60)
+
+
+main()
